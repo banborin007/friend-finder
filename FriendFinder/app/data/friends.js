84b3a1d7707795057d03aa8483cbca94f14b3d91
Bo <ban.borin@yahoo.com>
2018-10-24 16:49:54 -0700

    friend finder - not complete. Still need to do finish the html files and api route

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,54 @@
+// create possible friends
+// created the scores to the same and will edit once I get the survey and home done
+var friends = [
+    {
+        name: "Spongebob",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiPnNComaDeAhXlh1QKHX0QBx0QjRx6BAgBEAU&url=http%3A%2F%2Fspongebob.wikia.com%2Fwiki%2FSpongeBob_SquarePants_(character)&psig=AOvVaw24tcd4ww_0uhsJja-49AgG&ust=1540508980063904",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Patrick",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi1xNrwn6DeAhWqhlQKHf8zA8sQjRx6BAgBEAU&url=https%3A%2F%2Ftvtropes.org%2Fpmwiki%2Fpmwiki.php%2FCharacters%2FSpongebobSquarepantsPatrickStar&psig=AOvVaw3k1jRx7sO9XOMmx3fEfUMi&ust=1540510717909624",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Sandy",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi2kqTVoKDeAhXHGDQIHZHTAsYQjRx6BAgBEAU&url=http%3A%2F%2Ftheadventuresofgarythesnail.wikia.com%2Fwiki%2FSandy_Cheeks&psig=AOvVaw1qAn56GlxMkTS2EcemNzQV&ust=1540510885636950",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+];
+
+module.exports = friends;
